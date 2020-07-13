9a5f99642714b8ee24b5c4fb0f65a6b08a5b358b
unknown <bhavyasapra96@gmail.com>
2020-07-13 16:46:50 +0530

    Pushing updated Covid API code

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -10,7 +10,7 @@ let transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: 'bhavyasapra96@gmail.com',
-        pass: 'devrahababa96'
+        pass: '123'
     }
 });
 module.exports = {
